8cae36bd298d1ea9626531b4766e0af5bca2eca1
Tomasz Borys <t.borys@brs-software.pl>
2014-01-28 17:21:28 +0100

    added new PubliElementInterface and general implementation

---
+++ src/Brs/Stdlib/PublicElement.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * (c) BRS software - Tomasz Borys <t.borys@brs-software.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brs\Stdlib;
+
+/**
+ * @author Tomasz Borys <t.borys@brs-software.pl>
+ * @version 1.0 2013-01-24
+ */
+class PublicElement implements PublicElementInterface
+{
+    protected $serverPath;
+    protected $publicPath;
+
+    public function __construct($serverPath, $publicPath)
+    {
+        $this->serverPath = $serverPath;
+        $this->publicPath = $publicPath;
+    }
+
+    public function getServerPath()
+    {
+        return $this->serverPath;
+    }
+
+    public function getPublicPath()
+    {
+        return $this->publicPath;
+    }
+}
---
+++ src/Brs/Stdlib/PublicElementInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * (c) BRS software - Tomasz Borys <t.borys@brs-software.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brs\Stdlib;
+
+/**
+ * @author Tomasz Borys <t.borys@brs-software.pl>
+ * @version 1.0 2013-01-24
+ */
+interface PublicElementInterface
+{
+    public function getServerPath();
+    public function getPublicPath();
+}
