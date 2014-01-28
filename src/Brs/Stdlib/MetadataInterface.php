01133294e1657bf18ab0defa1c9bc0e323d58a44
Tomasz Borys <t.borys@brs-software.pl>
2014-01-28 17:20:33 +0100

    update method name

--- src/Brs/Stdlib/MetadataInterface.php
+++ src/Brs/Stdlib/MetadataInterface.php
@@ -5,6 +5,6 @@ namespace Brs\Stdlib;
 interface MetadataInterface
 {
     public function setMetadataFromProvider(MetadataProviderInterface $provider);
-    public function setMetadata(array $metaData);
+    public function setMetadata(array $metadata);
     public function getMetadata();
 }
--- src/Brs/Stdlib/MetadataTrait.php
+++ src/Brs/Stdlib/MetadataTrait.php
@@ -4,12 +4,12 @@ namespace Brs\Stdlib;
 
 trait MetadataTrait
 {
-    public function setMetaDataFromProvider(MetadataProviderInterface $provider)
+    public function setMetadataFromProvider(MetadataProviderInterface $provider)
     {
         $class = get_class($this);
 
         if ($provider->hasMetadataForClass($class)) {
-            $this->setMetaData($provider->getMetadataForClass($class));
+            $this->setMetadata($provider->getMetadataForClass($class));
         }
         return $this;
     }
