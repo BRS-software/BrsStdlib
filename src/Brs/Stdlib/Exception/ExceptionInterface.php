23993a223e16eac598ab5ec585bc2ea3c285e895
Tomasz Borys <t.borys@brs-software.pl>
2014-04-30 22:23:44 +0200

    Added exceptions for the module

---
+++ src/Brs/Stdlib/Exception/ExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * (c) BRS software - Tomasz Borys <t.borys@brs-software.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brs\Stdlib\Exception;
+
+/**
+ * @author Tomasz Borys <t.borys@brs-software.pl>
+ * @version 1.0 2013-04-26
+ */
+interface ExceptionInterface
+{
+}
---
+++ src/Brs/Stdlib/Exception/LogicException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * (c) BRS software - Tomasz Borys <t.borys@brs-software.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brs\Stdlib\Exception;
+
+/**
+ * @author Tomasz Borys <t.borys@brs-software.pl>
+ * @version 1.0 2013-04-26
+ */
+class LogicException extends \LogicException implements ExceptionInterface
+{}
