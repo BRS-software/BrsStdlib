4602d3b915d75e6e7e961d2de24c828ce1d8e341
tobo <t.borys@brs-software.pl>
2015-05-06 17:07:22 +0200

    new function used to move value inside the array

--- src/GeneralFunctions.php
+++ src/GeneralFunctions.php
@@ -53,6 +53,33 @@ function array_map_closure($array, Closure $dataFn, Closure $keyFn = null)
     return $result;
 }
 
+function array_move_value_by_index(array $array, $from = null, $to = null) {
+    if (null === $from) {
+        $from = count($array) - 1;
+    }
+    if (!isset($array[$from])) {
+        throw new Exception("Offset $from does not exist");
+    }
+    if (array_keys($array) != range(0, count($array) - 1)) {
+        throw new Exception("Invalid array keys");
+    }
+    if ($to < 0) {
+        throw new Exception("New offset cannot be a negative number");
+    }
+
+    $value = $array[$from];
+    unset($array[$from]);
+
+    if (null === $to) {
+        array_push($array, $value);
+    } else {
+        $tail = array_splice($array, $to);
+        array_push($array, $value);
+        $array = array_merge($array, $tail);
+    }
+    return $array;
+}
+
 function array_filter_custom($array, $toRemove, $recursive = false)
 {
     if (! is_array($toRemove) && (! $toRemove instanceof Closure)) {
